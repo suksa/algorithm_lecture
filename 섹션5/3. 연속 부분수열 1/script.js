7c41e64d3156bf3ee416aee7fc1121d0d11950df
suksa <suksa_@naver.com>
2023-07-19 23:34:16 +0900

    연속부분수열

--- 섹션5/3. 연속 부분수열 1/script.js
+++ 섹션5/3. 연속 부분수열 1/script.js
@@ -2,9 +2,28 @@ const { log } = console
 
 function solution(num, arr) {
     let answer = 0
+    let p1 = 0
+    let p2 = 1
     let temp = []
-    
-    
+    let sum = 0
+
+    for (let i = 0; i < arr.length - 1; i+=1) {
+        debugger
+        sum = arr.slice(i, p2 + 1).reduce((a, b) => a + b)
+
+        if (sum === num) {
+            answer += 1
+        }
+        while (sum < num) {
+            p2 += 1
+            sum = arr.slice(i, p2 + 1).reduce((a, b) => a + b)
+
+            if (sum === num) {
+                answer += 1
+                break
+            }
+        }
+    }
     return answer
 }
 
