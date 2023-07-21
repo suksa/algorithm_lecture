46097aaeba7749f8e84a0f0ae178d886c7f7fb51
suksa <suksa_@naver.com>
2023-07-21 23:47:10 +0900

    부분수열2

---
+++ 섹션5/4. 연속 부분수열 2/script.js
@@ -0,0 +1,17 @@
+const { log } = console
+
+function solution(num, arg) {
+    let answer = 0
+    let p1 = 0
+    let p2 = 1
+    let sum = 0
+
+    function repeat() {
+        
+    }
+    repeat()
+    
+    return answer
+}
+
+log(solution(5, [1,3,1,2,3]))
